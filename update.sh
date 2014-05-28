f9ba4500298ab8d42acf2a75746bf68f5417c508
enome <geert.pasteels@gmail.com>
2014-05-28 21:03:19 +0200

    0.10.0

---
+++ update.sh
@@ -0,0 +1,16 @@
+VERSION="0.10.0"
+
+curl -L "http://fb.me/react-with-addons-$VERSION.js" > react.js
+
+cat > component.json <<EOF
+{
+  "name": "react",
+  "repositiory": "enome/react",
+  "version": "$VERSION",
+  "description": "https://github.com/facebook/react",
+  "scripts": ["react.js"]
+}
+EOF
+
+git add .
+git commit -a -m "$VERSION"
